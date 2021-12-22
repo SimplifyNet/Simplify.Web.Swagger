5574b154c1a0673ae97bdd3cfdbae6b550628725
Alexanderius <alexander.krylkov@gmail.com>
2021-12-22 21:06:56 +0600

    [add] tester app

---
+++ src/TesterApp/Setup/IocRegistrations.cs
@@ -0,0 +1,17 @@
+﻿using Simplify.DI;
+using Simplify.Web.Bootstrapper;
+using Simplify.Web.Json;
+
+namespace TesterApp.Setup
+{
+	public static class IocRegistrations
+	{
+		public static IDIContainerProvider RegisterAll(this IDIContainerProvider containerProvider)
+		{
+			containerProvider.RegisterSimplifyWeb()
+				.RegisterJsonModelBinder();
+
+			return containerProvider;
+		}
+	}
+}
