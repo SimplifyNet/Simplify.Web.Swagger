39863368f97b811a6d6a7c73381c824e72701968
Alexanderius <alexander.krylkov@gmail.com>
2021-12-22 21:33:04 +0600

    [#1] [add] initial swagger middleware

--- src/TesterApp/Program.cs
+++ src/TesterApp/Program.cs
@@ -4,14 +4,21 @@ using Simplify.Web.Json.Model.Binding;
 using Simplify.Web.Model;
 using TesterApp.Setup;
 
+var builder = WebApplication.CreateBuilder(args);
+
 DIContainer.Current
-.RegisterAll()
-.Verify();
+	.RegisterAll()
+	.Verify();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 HttpModelHandler.RegisterModelBinder<JsonModelBinder>();
 
-var app = WebApplication.CreateBuilder(args).Build();
+var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseSimplifyWebWithoutRegistrations();
 
 app.Run();
