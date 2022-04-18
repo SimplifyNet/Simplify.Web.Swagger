a4569f2ae2bb0c431e29b1a71c0e02eda3264ada
Alexanderius <alexander.krylkov@gmail.com>
2022-04-18 23:40:27 +0600

    [#1] [add] initial SimplifyApiDescriptionGroupCollectionProvider and test controller

--- src/Simplify.Web.Swagger/SimplifyApiDescriptionGroupCollectionProvider.cs
+++ src/Simplify.Web.Swagger/SimplifyApiDescriptionGroupCollectionProvider.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Simplify.Web.Meta;
 
 namespace Simplify.Web.Swagger;
 
@@ -7,8 +10,24 @@ namespace Simplify.Web.Swagger;
 /// </summary>
 public class SimplifyApiDescriptionGroupCollectionProvider : IApiDescriptionGroupCollectionProvider
 {
+	private ApiDescriptionGroupCollection? _apiDescriptionGroups;
+
 	/// <summary>
 	/// Gets the API description groups
 	/// </summary>
-	public ApiDescriptionGroupCollection ApiDescriptionGroups => throw new global::System.NotImplementedException();
+	public ApiDescriptionGroupCollection ApiDescriptionGroups => _apiDescriptionGroups ??= LoadApiDescriptionGroup();
+
+	private ApiDescriptionGroupCollection LoadApiDescriptionGroup() => new ApiDescriptionGroupCollection(LoadApiDescriptionGroups(), 1);
+
+	private IReadOnlyList<ApiDescriptionGroup> LoadApiDescriptionGroups() =>
+		new List<ApiDescriptionGroup> { new ApiDescriptionGroup("Simplify.Web Controllers", LoadApiDescriptions()) };
+
+	private IReadOnlyList<ApiDescription> LoadApiDescriptions() => ControllersMetaStore.Current.ControllersMetaData.Select(CreateApiDescription).ToList();
+
+	private ApiDescription CreateApiDescription(IControllerMetaData item)
+	{
+		var desc = new ApiDescription();
+
+		return desc;
+	}
 }
--- src/TesterApp/Controllers/Api/v1/TestOutController.cs
+++ src/TesterApp/Controllers/Api/v1/TestOutController.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using Simplify.Web;
 using Simplify.Web.Attributes;
 
 namespace TesterApp.Controllers.Api.v1
 {
+	[Route("api/v{version:apiVersion}/weather-forecasts")]
+	[Produces("application/json")]
+	[ApiVersion("1.0")]
 	[Get("/api/v1/testOut")]
-	public class TestOutController : Controller
+	public class TestOutController : Simplify.Web.Controller
 	{
+		[HttpGet]
+		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 		public override ControllerResponse Invoke()
 		{
 			throw new NotImplementedException();
