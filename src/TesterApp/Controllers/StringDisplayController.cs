eca20b2aa3dba645fd2ab6e3adf2a3b72f7691d8
Alexanderius <alexander.krylkov@gmail.com>
2023-12-20 22:10:33 +0600

    [add] string parameter controller

---
+++ src/TesterApp/Controllers/StringDisplayController.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Mime;
+using Simplify.Web;
+using Simplify.Web.Attributes;
+using Simplify.Web.Swagger;
+
+namespace TesterApp.Controllers
+{
+	[Get("string-display/{Str}")]
+	[ProducesResponse(StatusCodes.Status200OK, MediaTypeNames.Text.Plain)]
+	public class StringDisplayController : Controller
+	{
+		public override ControllerResponse Invoke() => Content("User string: " + RouteParameters.Str);
+	}
+}
